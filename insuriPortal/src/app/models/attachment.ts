cda39fcb17edba8195fdae08de19d83143b86412
Dragos Dobre <dragos.dobre@swisstph.ch>
2019-11-02 15:58:40 +0100

    add patient model

---
+++ insuriPortal/src/app/models/attachment.ts
@@ -0,0 +1,11 @@
+export interface Attachment {
+    // from Element: extension
+    contentType: string; // Mime type of the content, with charset etc. 
+    language: string; // Human language of the content (BCP-47)
+    data: Blob; // Data inline, base64ed
+    url: string; // Uri where the data can be found
+    size: number; // Number of bytes of content (if url provided)
+    hash: Blob; // Hash of the data (sha-1, base64ed)
+    title: string; // Label to display in place of the data
+    creation: Date; // Date attachment was first created
+}
---
+++ insuriPortal/src/app/models/contactPoint.ts
@@ -0,0 +1,10 @@
+import { Period } from './Period';
+
+export interface ContactPoint {
+    // from Element: extension
+    system: string; // C? phone | fax | email | pager | url | sms | other
+    value: string; // The actual contact point details
+    use: string; // home | work | temp | old | mobile - purpose of this contact point
+    rank: number; // Specify preferred order of use (1 = highest)
+    period: Period; // Time period when the contact point was/is in use
+}
