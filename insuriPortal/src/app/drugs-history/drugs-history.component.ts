4091bfc5d1bfa9fc8d9b7121d49be63bc047a8b1
Vincent Perrin <vpcom01@gmail.com>
2019-11-03 11:04:35 +0100

    corrected missing declaration

--- insuriPortal/src/app/drugs-history/drugs-history.component.ts
+++ insuriPortal/src/app/drugs-history/drugs-history.component.ts
@@ -1,14 +1,17 @@
 import { ClaimService } from './../services/claim.service';
 import { Component, OnInit } from '@angular/core';
-import { Claim } from '../models/Claim';
 import { Observable } from 'rxjs';
-import {dateFormat} from '../global';
+import { Claim } from '../models/Claim';
+
+
 @Component({
   selector: 'app-drugs-history',
   templateUrl: './drugs-history.component.html',
   styleUrls: ['./drugs-history.component.scss']
 })
 export class DrugsHistoryComponent implements OnInit {
+
+  myClaimData$: Observable<Claim[]>;
 
   constructor(private ClaimService: ClaimService) { }
 
